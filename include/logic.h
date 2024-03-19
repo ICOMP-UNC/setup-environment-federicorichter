f7064cc1923a1b112c3f7db8ab5ad1c5f2c5796c
Federico Richter <federicorichter123@gmail.com>
2024-03-19 12:57:16 -0300

    Unit test included

---
+++ include/logic.h
@@ -0,0 +1,45 @@
+#pragma once
+
+/**
+ * @brief Add two numbers
+ * 
+ * 
+ * @param first_number
+ * @param second_number
+ * @return
+*/
+
+int add(int first_number, int second_number);
+
+/**
+ * @brief Add two numbers
+ * 
+ * 
+ * @param first_number
+ * @param second_number
+ * @return
+*/
+
+int subtract(int first_number, int second_number);
+
+/**
+ * @brief Add two numbers
+ * 
+ * 
+ * @param first_number
+ * @param second_number
+ * @return
+*/
+
+int multiply(int first_number, int second_number);
+
+/**
+ * @brief Add two numbers
+ * 
+ * 
+ * @param first_number
+ * @param second_number
+ * @return
+*/
+
+double divide(int first_number, int second_number);
